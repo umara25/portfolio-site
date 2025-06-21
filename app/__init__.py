d8d605a73734c2691ad43bed010ec3bff5ce6796
RobertXing <rxing24@uwo.ca>
2025-06-21 18:10:40 -0400

    Revert "feat: populate robert's about and hobby pages"
    This reverts commit b28e1174427a376edeb959cacbff96c7c7af25b0.

--- app/__init__.py
+++ app/__init__.py
@@ -10,31 +10,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-@app.route('/robert')
-def robert():
-    return render_template('about-me.html', title="Robert Xing", url=os.getenv("URL"),
-        image="./static/img/robert.jpg",
-        name="Robert Xing",
-        bio="Hi! I'm an incoming 4th year student at Western University, completing a dual degree in software engineering and Ivey HBA. "
-        "I am super excited to gain some PE experience and meet some incredibly talented people through the MLH Fellowship!",
+@app.route('/about')
+def about():
+    return render_template('about-me.html', title="About Me", url=os.getenv("URL"),
+        name="Name",
+        bio="bio",
         schools=[
-            {"school": "Western University", "program": "BESc Software Engineering and BA Honours Business Administration", "time": "Sep 2022-Apr 2027"}
+            {"school": "School Name", "program": "Program Name", "time": "YYYY-YYYY"}
         ],
         work_experiences=[
-            {"title": "Research Assistant, Data Scientist", "company": "Ivey Business School", "date": "Feb 2025-present"},
-            {"title": "Software Engineer Intern", "company": "Agora Tutoring", "date": "Feb 2025-Apr 2025"}
-        ]
-    )
-
-@app.route('/robert-hobbies')
-def robert_hobbies():
-    return render_template('hobbies.html', title="Robert's Hobbies", url=os.getenv("URL"),
-        name="Robert",
+            {"title": "Job Title", "company": "Company Name", "date": "YYYY-YYYY"}
+        ],
         hobbies=[
-            {"name": "Motorsports (F1 and WEC)", "image": "./static/img/wec.jpg"},
-            {"name": "Sports (Skiing, Golf, and Badminton)", "image": "./static/img/robert-ski.jpg"},
-            {"name": "Photography", "image": "./static/img/robert-photo.jpg"},
-            {"name": "Travel", "image": "./static/img/robert-travel.jpg"}
+            {"name": "Hobby Name", "image": "./static/img/placeholder.jpg"}
         ]
     )
 
