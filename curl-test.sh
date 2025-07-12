926b3a22250b6e455981460bae67679b22343ed1
Umar <umarahmer1@gmail.com>
2025-07-12 16:02:22 -0400

    Added a form that accepts inputs and makes a POST request

--- curl-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Umar&email=umarahmer1@gmail.com&content=this is a test'
-
-curl --request GET http://localhost:5000/api/timeline_post
-
-
