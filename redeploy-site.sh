496031caa173724b7b45d269770b3456bef9c1ff
Umar <umarahmer1@gmail.com>
2025-07-12 17:38:17 -0400

    updated redploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-tmux kill-server
-
-cd portfolio-site
+cd ~/portfolio-site
 
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
+
 pip install -r requirements.txt
 
-tmux new -d -s redeploy-portfolio 'cd portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
+
+
