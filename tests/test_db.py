90662640a1162ed322b2d37eafbf25083124f7e9
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-07-17 11:23:19 -0500

    Update unittest assertion methods for improved error messages - Error messages will now be more detailed

--- tests/test_db.py
+++ tests/test_db.py
@@ -26,16 +26,16 @@ class TestTimelinePost(unittest.TestCase):
         first_post = TimelinePost.create(
             name="John Doe", email="john@example.com", content="Hello world, I'm John!"
         )
-        assert first_post.id == 1
+        self.assertEqual(first_post.id, 1)
         second_post = TimelinePost.create(
             name="Jane Doe", email="jane@example.com", content="Hellow world, I'm Jane!"
         )
-        assert second_post.id == 2
+        self.assertEqual(second_post.id, 2)
 
         all_posts = TimelinePost.select().order_by(TimelinePost.created_at.desc())
 
-        assert len(all_posts) == 2
+        self.assertEqual(len(all_posts), 2)
         for posts in all_posts:
-            assert len(posts.name) != 0
-            assert len(posts.email) != 0
-            assert len(posts.content) != 0
+            self.assertNotEqual(len(posts.name), 0)
+            self.assertNotEqual(len(posts.email), 0)
+            self.assertNotEqual(len(posts.content), 0)
