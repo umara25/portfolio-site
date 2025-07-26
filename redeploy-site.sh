ffe7b1b664fbfe34839fd897845a853591717599
Umar <77904387+umara25@users.noreply.github.com>
2025-07-26 16:46:24 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,6 +4,6 @@ cd ~/portfolio-site
 
 git fetch && git reset origin/main --hard
 
-docker compose -f docker-compose.prod.yml dow
+docker compose -f docker-compose.prod.yml down
 
 docker compose -f docker-compose.prod.yml up -d --build
